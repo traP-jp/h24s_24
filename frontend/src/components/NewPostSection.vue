4a1bc8fe7a5bc7fc0f8d23f78c8b7d987f11ced4
AlteraKlam <gina0528gle@gmail.com>
2024-06-15 12:57:46 +0900

    基本の型だけ

---
+++ frontend/src/components/NewPostSection.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+import Button from "./Button.vue"
+import Avatar from "./Avatar.vue"
+
+</script>
+
+<template>
+    <div class="newPostSection">
+        <div class="newPostInputSection">        
+            <Avatar name="" size="30px"></Avatar>
+            <input  type="text" placeholder="投稿する内容を入力（投稿時に自動で変換されます">
+        </div>
+        <div align="right">
+            <Button>投稿する</Button>
+        </div>
+    </div>
+</template>
+
+<style>
+.newPostSection{
+    width: 500px;
+    height: 300px;
+    background-color: white;
+    margin: 10px
+}
+.newPostInputSection{
+    width:250px;
+    height:50px;
+    background-color:white;
+}
+Button {
+    display: flex;
+    align-items: flex-start;
+    background-color: #F4A344;
+    color: white; 
+    padding: 8px 16px;
+    border: none;
+    border-radius: 10px;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+    margin: 10px;
+    justify-content: flex-end;
+}
+</style>
