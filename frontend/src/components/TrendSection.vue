3e1fc942ccf5353fdc456d1b1fdcc5d4db9e046c
AlteraKlam <gina0528gle@gmail.com>
2024-06-15 15:03:07 +0900

    上のボタンだけ（途中１）

---
+++ frontend/src/components/TrendSection.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Avatar from './Avatar.vue'
+import { ref } from 'vue'
+
+const buttonCls = ref('reaction-button')
+
+</script>
+
+<template>
+    <div class="trendSection" >
+        <button :class="buttonCls"> ❤️ </button>
+        <button :class="buttonCls"> 🔥 </button>
+        <button :class="buttonCls"> 💧 </button>
+        <button :class="buttonCls"> 😭 </button>
+        <button :class="buttonCls"> 🤔 </button>
+    </div>
+</template>
+
+<style>
+.reaction-button{
+    width: 50px;
+    border:none;
+}
+.selected-button{
+    width:50px;
+}
+
+</style>
