b200fe53db6c11d66b034fcf321a96be2e7eb6c6
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-06-15 12:38:03 +0900

    :wrench: NS用の環境変数設定

--- backend/repository/db.go
+++ backend/repository/db.go
@@ -13,10 +13,10 @@ func NewDB() (*sqlx.DB, error) {
 	jst, _ := time.LoadLocation("Asia/Tokyo")
 	mysqlConf := mysql.Config{
 		Net:                  "tcp",
-		User:                 cmp.Or(os.Getenv("DB_USER"), "root"),
-		Passwd:               cmp.Or(os.Getenv("DB_PASSWORD"), "passsword"),
-		Addr:                 cmp.Or(os.Getenv("DB_HOST"), "db") + ":" + cmp.Or(os.Getenv("DB_PORT"), "3306"),
-		DBName:               cmp.Or(os.Getenv("DB_NAME"), "h24s_24"),
+		User:                 cmp.Or(os.Getenv("DB_USER"), os.Getenv("NS_MARIADB_USER"), "root"),
+		Passwd:               cmp.Or(os.Getenv("DB_PASSWORD"), os.Getenv("NS_MARIADB_PASSWORD"), "passsword"),
+		Addr:                 cmp.Or(os.Getenv("DB_HOST"), os.Getenv("NS_MARIADB_HOSTNAME"), "db") + ":" + cmp.Or(os.Getenv("DB_PORT"), os.Getenv("NS_MARIADB_PORT"), "3306"),
+		DBName:               cmp.Or(os.Getenv("DB_NAME"), os.Getenv("NS_MARIADB_DATABASE"), "h24s_24"),
 		Loc:                  jst,
 		AllowNativePasswords: true,
 		ParseTime:            true,
