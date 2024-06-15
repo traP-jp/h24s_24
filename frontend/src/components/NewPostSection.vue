1fcc0c093320034e5ba2652fd102af7e7bac54a1
AlteraKlam <gina0528gle@gmail.com>
2024-06-15 17:11:55 +0900

    再々修正

--- frontend/src/components/NewPostSection.vue
+++ frontend/src/components/NewPostSection.vue
@@ -18,7 +18,7 @@ const ButtonVisible = ref(false)
     </div>
     <div class="new-post-input-section">
       <input type="text" placeholder="投稿する内容を入力（投稿時に自動で変換されます)" v-model="inputContext"
-             @input="ButtonVisible = (inputContext.length == 0) || (inputContext.length > 5) ? false : true "> 
+             @input="ButtonVisible = (inputContext.length == 0) || (inputContext.length > 280) ? false : true "> 
       <p v-if="!ButtonVisible">文字数の上限は280文字です</p>
       <div class="post-button" align="right">
         <Button id="canPost" :enabled="ButtonVisible">投稿する</Button>
