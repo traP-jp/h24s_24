0583fdf744f381ec258011190222bcf7333c3f98
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-06-15 09:48:51 +0900

    size変更可能にした・marginを消した・空行追加

--- frontend/src/components/Avatar.vue
+++ frontend/src/components/Avatar.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 defineProps<{
-  name: string
+  name: string,
+  size: string
 }>()
 const endpoint = "https://q.trap.jp/api/v3"
 </script>
 
 <template>
-<img class="avatar" :src="`${endpoint}/public/icon/${name}`" />
+<img class="avatar" :src="`${endpoint}/public/icon/${name}`" :style="{width: size, height: size}" />
 </template>
 
 <style scoped>
 .avatar {
-  width: 40px;
-  height: 40px;
   border-radius: 50%;
-  margin: 10px;
 }
 </style>
