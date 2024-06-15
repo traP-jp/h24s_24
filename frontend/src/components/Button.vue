088ac8db5c89a7e55e202039bba2bb033f49ae2e
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-06-15 14:56:11 +0900

    CSSを整えた (ついでにButtonにenabledプロパティを作った)

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  enabled: boolean
+}>()
+</script>
+
 <template>
-    <button type="button"><slot></slot></button>
+    <button type="button" :class="enabled ? 'enabled' : undefined"><slot></slot></button>
 </template>
+
+<style lang="scss" scoped>
+button {
+  color: white;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+  &.enabled {
+    background-color: var(--accent-color);
+  }
+}
+</style>
--- frontend/src/components/NewPostSection.vue
+++ frontend/src/components/NewPostSection.vue
@@ -2,69 +2,62 @@
 
 import Button from "./Button.vue"
 import Avatar from "./Avatar.vue"
-import { ref } from "vue"
+import {ref} from "vue"
 
 const inputContext = ref('')
 let ButtonVisible = ref(true)
 
 function checkLength() {
-    if (inputContext.value.length > 280){
-        ButtonVisible.value = false
-    } else {
-        ButtonVisible.value = true
-    }
+  if (inputContext.value.length > 280) {
+    ButtonVisible.value = false
+  } else {
+    ButtonVisible.value = true
+  }
 }
 
 </script>
 
 <template>
-    <div class="newPostSection">
-        <div class="newPostInputSection">        
-            <Avatar name="" size="50px"></Avatar>
-            <input  type="text" placeholder="投稿する内容を入力（投稿時に自動で変換されます)" v-model="inputContext" @input="checkLength">
-            <p v-if="!ButtonVisible">文字数の上限は280文字です</p>
-        </div>
-        <div align="right">
-            <Button v-if="ButtonVisible" id="canPost">投稿する</Button>
-            <Button v-if="!ButtonVisible" id="cannotPost">投稿する</Button>
-        </div>
+  <div class="newPostSection">
+    <div class="author-icon">
+      <Avatar name="" size="48px"></Avatar>
     </div>
+    <div class="newPostInputSection">
+      <input type="text" placeholder="投稿する内容を入力（投稿時に自動で変換されます)" v-model="inputContext"
+             @input="checkLength">
+      <p v-if="!ButtonVisible">文字数の上限は280文字です</p>
+      <div class="post-button" align="right">
+        <Button id="canPost" :enabled="ButtonVisible">投稿する</Button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-.newPostSection{
-    width: 500px;
-    height: 300px;
-    background-color: white;
-    margin: 10px
+<style lang="scss" scoped>
+.newPostSection {
+  width: 480px;
+  padding: 16px;
+  display: flex;
+
+  .newPostInputSection {
+    width: 100%;
+    margin-left: 8px;
+
+    input {
+      display: block;
+      width: 100%;
+      border: none;
+      padding-top: 16px;
+      padding-bottom: 8px;
+
+      &:focus-visible {
+        outline: none;
+      }
+    }
+    .post-button {
+      margin: 8px;
+    }
+  }
 }
-.newPostInputSection{
-    width:250px;
-    height:50px;
-    background-color:white;
-}
-#canPost {
-    background-color: #F4A344;
-    color: white; 
-    padding: 8px 16px;
-    border: none;
-    border-radius: 10px;
-    font-size: 14px;
-    font-weight: bold;
-    text-align: center;
-    cursor: pointer;
-    margin: 10px;
-}
-#cannotPost {
-    background-color: gainsboro;
-    color: white; 
-    padding: 8px 16px;
-    border: none;
-    border-radius: 10px;
-    font-size: 14px;
-    font-weight: bold;
-    text-align: center;
-    cursor: pointer;
-    margin: 10px;
-}
+
 </style>
