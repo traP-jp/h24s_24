416f5ae12a43ee1da297040628d41a43196831c9
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-06-15 09:56:10 +0900

    sizeをv-bindを使ってCSS内で反映するように変更

--- frontend/src/components/Avatar.vue
+++ frontend/src/components/Avatar.vue
@@ -7,11 +7,13 @@ const endpoint = "https://q.trap.jp/api/v3"
 </script>
 
 <template>
-<img class="avatar" :src="`${endpoint}/public/icon/${name}`" :style="{width: size, height: size}" />
+<img class="avatar" :src="`${endpoint}/public/icon/${name}`" />
 </template>
 
 <style scoped>
 .avatar {
+  width: v-bind(size);
+  height: v-bind(size);
   border-radius: 50%;
 }
 </style>
