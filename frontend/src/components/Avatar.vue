baee46ec0dfebf3f0e64dbb4ad626f2e2a478645
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-06-15 09:27:21 +0900

    Avatarコンポーネント追加(#6)

---
+++ frontend/src/components/Avatar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+const endpoint = "https://q.trap.jp/api/v3"
+</script>
+
+<template>
+<img class="avatar" :src="`${endpoint}/public/icon/${name}`" />
+</template>
+
+<style scoped>
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin: 10px;
+}
+</style>
