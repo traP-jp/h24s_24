fa911e7cedaa67eb4117f8a235f6f223de4f8146
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-06-16 10:54:09 +0900

    /posts/:idに移動

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -20,7 +20,7 @@ const router = createRouter({
       component: () => import('@/views/ProfileView.vue'),
     },
     {
-      path: '/post/:id',
+      path: '/posts/:id',
       name: 'post',
       component: () => import('@/views/PostView.vue'),
     },
