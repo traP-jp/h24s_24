1c3e9b0d480feca5aee64f61e678bd91e7f06c70
cp-20 <n.naoki.1587@gmail.com>
2024-06-17 00:02:07 +0900

    fix: 親投稿が正しく取得されないバグの修正

--- backend/repository/post.go
+++ backend/repository/post.go
@@ -195,12 +195,29 @@ func (pr *PostRepository) GetAncestors(ctx context.Context, postID uuid.UUID) ([
 		return nil, fmt.Errorf("post not found: %w", err)
 	}
 
+	if p.ID == p.ParentID {
+		return []*domain.Post{}, nil
+	}
+
 	var posts []post
-	err = pr.db.Select(&posts,
-		"SELECT * FROM posts WHERE root_id = ? AND created_at < ? ORDER BY created_at ASC", p.RootID, p.CreatedAt)
+	err = pr.db.Select(&posts, 
+		"WITH RECURSIVE parent_cte AS (" +
+			"SELECT id, user_name, original_message, converted_message, parent_id, root_id, created_at "+
+			"FROM posts "+
+			"WHERE id = ? "+
+			"UNION ALL "+
+			"SELECT p.id AS `id`, p.user_name AS `user_name`, p.original_message AS `original_message`, p.converted_message AS `converted_message`, p.parent_id AS `parent_id`, p.root_id AS `root_id`, p.created_at AS `created_at` "+
+			"FROM posts p JOIN parent_cte c ON p.id = c.parent_id "+
+			"WHERE p.id <> p.parent_id"+
+		") "+
+		"SELECT * FROM parent_cte UNION ALL "+
+		"SELECT id, user_name, original_message, converted_message, parent_id, root_id, created_at "+
+		"FROM posts WHERE id = ? AND id <> ? AND id <> ?;", p.ParentID, p.RootID, p.ID, p.ParentID)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get ancestors: %w", err)
 	}
+
+	slices.Reverse(posts)	
 
 	domainPosts := make([]*domain.Post, 0, len(posts))
 	for _, p := range posts {
