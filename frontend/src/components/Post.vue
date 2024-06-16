aaaa39a54fd4c71c4674302f38cae3c23cee8ee5
ZOI_dayo <zoizoi.dayo@gmail.com>
2024-06-16 15:25:32 +0900

    必要なコードを消していたので修復

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -2,9 +2,9 @@
 import Avatar from '@/components/Avatar.vue';
 import 'moment/dist/locale/ja';
 import moment from 'moment-timezone';
-import { effect, ref } from 'vue';
-import { reactionIcons } from '@/features/reactions';
-import { deleteReaction, postReaction } from '@/features/api';
+import {effect, ref} from 'vue';
+import {reactionIcons} from '@/features/reactions';
+import {deleteReaction, postReaction} from '@/features/api';
 import twemoji from 'twemoji';
 
 type Reaction = { id: number; count: number; clicked: boolean };
@@ -58,38 +58,40 @@ const vTwemoji = {
 </script>
 
 <template>
-  <div class="post">
-    <div class="post-author-icon">
-      <Avatar size="48px" :name="name" />
-    </div>
-    <div class="post-content">
-      <div class="post-header">
-        <span class="post-author">@{{ name }}</span>
-        <span class="post-date">{{ dateText }}</span>
+  <router-link :to="`/posts/${id}/`" class="post-link">
+    <div class="post">
+      <div class="post-author-icon">
+        <Avatar size="48px" :name="name"/>
       </div>
-      <div class="post-message">
-        {{ content }}
-      </div>
-      <div class="post-reactions">
-        <button
-          v-for="reaction in copiedReactions"
-          :key="reaction.id"
-          class="post-reaction"
-          :class="{ clicked: reaction.clicked, ripple: newReaction === reaction.id }"
-          @click="
+      <div class="post-content">
+        <div class="post-header">
+          <span class="post-author">@{{ name }}</span>
+          <span class="post-date">{{ dateText }}</span>
+        </div>
+        <div class="post-message">
+          {{ content }}
+        </div>
+        <div class="post-reactions">
+          <button
+              v-for="reaction in copiedReactions"
+              :key="reaction.id"
+              class="post-reaction"
+              :class="{ clicked: reaction.clicked, ripple: newReaction === reaction.id }"
+              @click="
             (e) => {
               toggleReaction(reaction);
               e.stopPropagation();
               e.preventDefault();
             }
           "
-        >
-          <span class="post-reaction-icon" v-twemoji>{{ reactionIcons[reaction.id] }}</span>
-          <span class="post-reaction-count">{{ reaction.count }}</span>
-        </button>
+          >
+            <span class="post-reaction-icon" v-twemoji>{{ reactionIcons[reaction.id] }}</span>
+            <span class="post-reaction-count">{{ reaction.count }}</span>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <style lang="scss" scoped>
