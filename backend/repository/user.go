e89faa746670ffa44064acbe2bf3db7bfcca2a29
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-06-16 11:01:23 +0900

    :bug: GetUserPostsを修正

--- backend/repository/user.go
+++ backend/repository/user.go
@@ -16,13 +16,26 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 }
 
 func (ur *UserRepository) GetUserPosts(userName string) ([]*domain.Post, error) {
-	var userPosts []*domain.Post
+	var userPosts []post
 	err := ur.DB.Select(&userPosts, "SELECT * FROM posts WHERE user_name = ? ORDER BY created_at DESC", userName)
 	if err != nil {
 		return nil, err
 	}
 
-	return userPosts, nil
+	posts := make([]*domain.Post, 0, len(userPosts))
+	for _, post := range userPosts {
+		posts = append(posts, &domain.Post{
+			ID:               post.ID,
+			UserName:         post.UserName,
+			OriginalMessage:  post.OriginalMessage,
+			ConvertedMessage: post.ConvertedMessage,
+			ParentID:         post.ParentID,
+			RootID:           post.RootID,
+			CreatedAt:        post.CreatedAt,
+		})
+	}
+
+	return posts, nil
 }
 
 func (ur *UserRepository) GetUserReactionCount(userName string) (int, int, error) {
