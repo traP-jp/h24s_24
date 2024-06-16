58d5941cf0da3dcb8adfdb48b69a33985c18fd21
cp-20 <n.naoki.1587@gmail.com>
2024-06-16 13:13:17 +0900

    feat: 投稿した後のローディングを実装

--- frontend/src/components/NewPostSection.vue
+++ frontend/src/components/NewPostSection.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Button from './Button.vue';
 import Avatar from './Avatar.vue';
+import Loader from '@/components/Loader.vue';
+
 import { ref, computed } from 'vue';
 import { createPost } from '@/features/api';
 
@@ -16,11 +18,15 @@ const inputContent = ref('');
 const canPost = computed(() => {
   return inputContent.value.length != 0 && inputContent.value.length <= 280;
 });
-const post = () => {
-  createPost({
+const loading = ref(false);
+
+const post = async () => {
+  loading.value = true
+  await createPost({
     message: inputContent.value,
     parent_id: props.parentId,
   });
+  loading.value = false;
   emit('submit');
   inputContent.value = '';
 };
@@ -32,17 +38,16 @@ const post = () => {
       <Avatar :name="name" size="48px"></Avatar>
     </div>
     <div class="new-post-input-section">
-      <textarea
-        type="text"
-        :placeholder="`${parentId == undefined ? '投稿' : '返信'}する内容を入力（投稿時に自動で変換されます)`"
-        v-model="inputContent"
-      />
+      <div v-if="loading" class="new-post-input-section-loader">
+        <Loader />
+      </div>
+      <textarea type="text" :placeholder="`${parentId == undefined ? '投稿' : '返信'}する内容を入力（投稿時に自動で変換されます)`"
+        v-model="inputContent" :disabled="loading" />
       <div class="post-footer">
         <span :class="{ 'post-charcount-warn': !canPost }">{{ inputContent.length }}/280文字</span>
         <span class="post-button">
-          <Button :disabled="!canPost" :onclick="post">
-            {{ parentId == undefined ? '投稿' : '返信' }}する</Button
-          >
+          <Button :disabled="!canPost || loading" :onclick="post">
+            {{ parentId == undefined ? '投稿' : '返信' }}する</Button>
         </span>
       </div>
     </div>
@@ -58,6 +63,16 @@ const post = () => {
   .new-post-input-section {
     width: calc(100% - 56px);
     margin-left: 8px;
+    position: relative;
+
+    .new-post-input-section-loader {
+      position: absolute;
+      inset: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff8;
+    }
 
     textarea {
       display: block;
